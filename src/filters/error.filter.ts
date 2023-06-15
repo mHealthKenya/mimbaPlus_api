19f952d62dd6284e0ac3de8dc0aa21cbeb6822bb
JoelWekesa <joelwekesa.jw@gmail.com>
2023-06-16 01:01:30 +0300

    Prisma Error Filter

--- src/filters/error.filter.ts
+++ src/filters/error.filter.ts
@@ -18,6 +18,123 @@ export class CustomErrorFilter implements ExceptionFilter {
       error?.response?.message || error?.message || 'Internal server error';
 
     switch (error?.response?.code) {
+      case '1000':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Authentication failed against database server',
+        });
+        break;
+
+      case '1001':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: "Can't reach database server",
+        });
+        break;
+
+      case '1002':
+      case '1008':
+      case '2024':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Connection timeout',
+        });
+        break;
+
+      case '1003':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Invalid database connection',
+        });
+        break;
+
+      case '1009':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Duplicate error',
+        });
+        break;
+
+      case '1010':
+      case '1011':
+      case '1013':
+      case '1017':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Invalid connection',
+        });
+        break;
+
+      case '1012':
+      case '1015':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Upgrade error',
+        });
+        break;
+
+      case '1014':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Invalid model',
+        });
+        break;
+
+      case '1016':
+      case '2000':
+      case '2005':
+      case '2006':
+      case '2007':
+      case '2009':
+      case '2010':
+      case '2011':
+      case '2012':
+      case '2013':
+      case '2016':
+      case '2019':
+      case '2020':
+      case '2021':
+      case '2022':
+      case '2023':
+      case '2026':
+      case '2027':
+      case '2028':
+      case '2030':
+      case '2031':
+      case '2033':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Invalid input',
+        });
+        break;
+
+      case '2001':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Not found exception',
+        });
+        break;
+
       case 'P2002':
         response.status(status).json({
           statusCode: status,
@@ -27,12 +144,26 @@ export class CustomErrorFilter implements ExceptionFilter {
         });
         break;
 
+      case 'P2003':
+      case 'P2004':
+      case 'P2014':
+      case '2017':
+      case '2018':
+        response.status(status).json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Could not complete request due to relationship conflicts',
+        });
+        break;
+
+      case 'P2015':
       case 'P2025':
         response.status(status).json({
           statusCode: status,
           timestamp: new Date().toISOString(),
           path: request.url,
-          message: 'Record not found',
+          message: 'Related record could not be found',
         });
         break;
 
@@ -47,14 +178,12 @@ export class CustomErrorFilter implements ExceptionFilter {
         });
         break;
 
-      case 'P2016':
-      case 'P2017':
-      case 'P2018':
+      case 'P2034':
         response.status(status).json({
           statusCode: status,
           timestamp: new Date().toISOString(),
           path: request.url,
-          message: 'Invalid data',
+          message: 'Transaction failed',
         });
         break;
 
