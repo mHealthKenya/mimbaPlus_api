189373a150de3d1306ce5ea2f1370e72888da577
Guyoguyes <g.abduba43@gmail.com>
2024-04-29 11:25:44 +0300

    wallet controller

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
@@ -6,6 +6,41 @@ import { UpdateWalletDto } from './dto/update-wallet.dto';
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
+
+  @Post()
+  async createWallet(@Body() createWalletDto: CreateWalletDto) {
+    return this.walletService.createWallet(createWalletDto);
+  }
+
+  @Post()
+  async generateToken(userId: string, amount: number) {
+    const newBalance = await this.walletService.generateToken(userId, amount);
+    return { newBalance };
+  }
+
+  @Get('balance/:walletId')
+  async getWalletBalance(@Param('walletId') walletId: string) {
+    return this.walletService.getWalletBalance(walletId);
+  }
+
+
+  @Get(':userId')
+  async getWalletByUserId(@Param('userId') userId: string) {
+    const wallet = await this.walletService.getWalletByUserId(userId);
+    if (!wallet) {
+      throw new NotFoundException('Wallet not found');
+    }
+    return wallet;
+  }
+
+  @Patch('transfer/:userId/:facilityId/:amount')
+  async transferTokenFromMotherToFacility(
+    @Param('userId') userId: string,
+    @Param('facilityId') facilityId: string,
+    @Param('amount') amount: number,
+  ) {
+    return this.walletService.transferTokenFromMotherToFacility(userId, facilityId, amount);
+  }
 
   
 }
