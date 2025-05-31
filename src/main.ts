7e226f59218fee2449821c7541f208b513d0f083
Joel Wekesa <joel.wekesa@oldmutual.com>
2025-05-31 11:52:43 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import { CustomErrorFilter } from './filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://mwebtest.mhealthkenya.org', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomErrorFilter());
   await app.listen(process.env.PORT || 8000);
