a939d10a1205821ec5d075027d76934a4e8b9a08
Joel Wekesa <joel.wekesa@oldmutual.com>
2025-05-31 11:13:31 +0300

    fix: auth

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { CustomErrorFilter } from './filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://mwebtest.mhealthkenya.org', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomErrorFilter());
   await app.listen(process.env.PORT || 8000);
