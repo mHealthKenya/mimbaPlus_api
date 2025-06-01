c8b9aef75f48a371471c54d9d4fa7d45f660e6f4
Joel Wekesa <joel.wekesa@oldmutual.com>
2025-06-01 19:20:21 +0300

    test: Test auto deployment to dev environment

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Docker build successful! Welcome to the NestJS application.";
+    return "Testing auto deployment to the dev environment";
   }
 }
