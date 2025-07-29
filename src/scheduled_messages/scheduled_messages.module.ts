6871c60f9a9b5bc2f5f238038e1a73e0602aa3e1
GuyoDev254 <g.abduba43@gmail.com>
2025-07-29 14:56:15 +0300

    scheduled messages

--- src/scheduled_messages/scheduled_messages.module.ts
+++ src/scheduled_messages/scheduled_messages.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ScheduledMessagesService } from './scheduled_messages.service';
 import { ScheduledMessagesController } from './scheduled_messages.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SendsmsService } from 'src/sendsms/sendsms.service';
 import { DatePicker } from 'src/helpers/date-picker';
 import { UserHelper } from 'src/helpers/user-helper';
+import { SendsmsController } from 'src/sendsms/sendsms.controller';
+import { AuthMiddleware } from 'src/middleware/auth/auth.middleware';
 
 @Module({
   controllers: [ScheduledMessagesController],
   providers: [ScheduledMessagesService, PrismaService, SendsmsService, DatePicker, UserHelper]
 })
-export class ScheduledMessagesModule {}
+export class ScheduledMessagesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(SendsmsController);
+  }
+}
